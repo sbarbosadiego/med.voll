cf9b1a50d22be58a1d095c0aa2727413f5a9916c
Diego Barbosa <diego.barbosa.pro@gmail.com>
2025-11-24 23:22:26 -0300

    Sobe

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,7 +1,13 @@
 package med.voll.api.controller;
 
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacienteController {
 
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println(dados);
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
     }
 
 }
