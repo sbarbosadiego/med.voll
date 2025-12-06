4bf40a9872d729805e73f1b43750fc6d30b433e0
Diego Barbosa <diego.barbosa.pro@gmail.com>
2025-12-06 19:01:36 -0300

    Sobe

---
+++ src/main/java/med/voll/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,9 @@
+package med.voll.api.paciente;
+
+public record DadosDetalhamentoPaciente(Long id, String nome, String email, String cpf, String telefone) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone());
+    }
+
+}
