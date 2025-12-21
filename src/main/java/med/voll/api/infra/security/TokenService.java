686bdae0044c94c8501bf2bcecbd99804f6bbc0e
Diego Barbosa <diego.barbosa.pro@gmail.com>
2025-12-20 23:42:27 -0300

    Sobe

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,7 +1,14 @@
+package med.voll.api.infra.security;
+
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuario.Usuario;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
